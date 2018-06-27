16b4411c147adff99400cb1520d5e6af23a7e905
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-06-27 23:55:05 +0530

    updated

--- pluggable-generic-batch-component-demo-service/src/main/java/alpha/batch/component/prototype/demo/DemoApp.java
+++ pluggable-generic-batch-component-demo-service/src/main/java/alpha/batch/component/prototype/demo/DemoApp.java
@@ -1,9 +1,12 @@
 package alpha.batch.component.prototype.demo;
 
 import alpha.batch.component.prototype.AlphaBatchContext;
+import alpha.batch.component.prototype.demo.domain.Alpha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.RowMapper;
 
 /**
  * @author narayana
@@ -14,5 +17,15 @@ public class DemoApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApp.class, args);
+	}
+
+
+	@Bean
+	public RowMapper<Alpha> alphaRowMapper() {
+		return (rs, rn) -> new Alpha() {{
+			setId(rs.getString("id"));
+			setName(rs.getString("name"));
+			setCity(rs.getString("city"));
+		}};
 	}
 }

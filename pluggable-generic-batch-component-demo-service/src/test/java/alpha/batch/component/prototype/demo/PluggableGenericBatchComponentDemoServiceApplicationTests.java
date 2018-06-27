f621939577d04913f81394583ce1987f0428b017
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-06-27 23:27:25 +0530

    Initial commit

---
+++ pluggable-generic-batch-component-demo-service/src/test/java/alpha/batch/component/prototype/demo/PluggableGenericBatchComponentDemoServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package alpha.batch.component.prototype.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PluggableGenericBatchComponentDemoServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

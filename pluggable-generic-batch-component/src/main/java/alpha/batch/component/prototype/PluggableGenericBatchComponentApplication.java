067a71909307a01295ad5150a39cb0447feb8616
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-06-27 18:57:55 +0530

    Initial commit

---
+++ pluggable-generic-batch-component/src/main/java/alpha/batch/component/prototype/PluggableGenericBatchComponentApplication.java
@@ -0,0 +1,12 @@
+package alpha.batch.component.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluggableGenericBatchComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluggableGenericBatchComponentApplication.class, args);
+	}
+}
---
+++ pluggable-generic-batch-component/src/test/java/alpha/batch/component/prototype/PluggableGenericBatchComponentApplicationTests.java
@@ -0,0 +1,16 @@
+package alpha.batch.component.prototype;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PluggableGenericBatchComponentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

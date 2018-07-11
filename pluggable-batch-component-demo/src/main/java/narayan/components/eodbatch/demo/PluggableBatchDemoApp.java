e7d58600ae51f3e30c2dc2b4a625045156c940bb
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-07-12 00:30:00 +0530

    Done with initial version

--- pluggable-batch-component-demo/src/main/java/narayan/components/eodbatch/demo/PluggableBatchDemoApp.java
+++ pluggable-batch-component-demo/src/main/java/narayan/components/eodbatch/demo/PluggableBatchDemoApp.java
@@ -1,8 +1,14 @@
 package narayan.components.eodbatch.demo;
 
 import narayan.components.eodbatch.EnableEODBatchProcessing;
+import narayan.components.eodbatch.demo.domain.Alpha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.PreparedStatement;
 
 /**
  *
@@ -15,5 +21,23 @@ public class PluggableBatchDemoApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PluggableBatchDemoApp.class, args);
+	}
+
+	@Bean(name = "alphaRowMapper")
+	public RowMapper<Alpha> alphaRowMapper() {
+		return (rs, rn) -> new Alpha() {{
+			setId(rs.getInt("id"));
+			setName(rs.getString("name"));
+			setCity(rs.getString("city"));
+			setCountry(rs.getString("country"));
+		}};
+	}
+
+	@Bean(name="alphaPSS")
+	public PreparedStatementSetter alphaPSS() {
+		return (PreparedStatement ps) -> {
+			ps.setInt(1, 2);
+			ps.setInt(2, 4);
+		};
 	}
 }
---
+++ pluggable-batch-component-demo/src/main/java/narayan/components/eodbatch/demo/domain/Alpha.java
@@ -0,0 +1,46 @@
+package narayan.components.eodbatch.demo.domain;
+
+/**
+ *
+ * @author narayana
+ *
+ */
+public class Alpha {
+
+    private Integer id;
+    private String name;
+    private String city;
+    private String country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}

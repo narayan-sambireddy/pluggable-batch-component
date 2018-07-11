6b708f340aaea208ecda5b3db6b43b4240548ed8
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-07-11 23:32:44 +0530

    Initial commit of demo

---
+++ pluggable-batch-component-demo/src/main/java/narayan/components/eodbatch/demo/PluggableBatchComponentDemoApplication.java
@@ -0,0 +1,12 @@
+package narayan.components.eodbatch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluggableBatchComponentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluggableBatchComponentDemoApplication.class, args);
+	}
+}
---
+++ pluggable-batch-component-demo/src/test/java/narayan/components/eodbatch/demo/PluggableBatchComponentDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package narayan.components.eodbatch.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PluggableBatchComponentDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
